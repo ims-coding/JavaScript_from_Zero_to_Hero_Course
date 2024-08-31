8cd16cdfc05af42d6c09afa92f9ba0f411d0b869
myselfims <shaikhimran7585@gmail.com>
2024-08-31 18:45:03 +0530

    data types in javascript

---
+++ data types/data_types.js
@@ -0,0 +1,39 @@
+
+
+let age = 22;
+console.log(age, typeof(age))
+
+let name = "John Deo"
+let name2 = 'John Deo'
+let name3 = `John Deo`
+console.log(name, typeof(name))
+
+let hasPermission = true
+console.log(hasPermission, typeof(hasPermission))
+
+let user 
+console.log(user, typeof(user))
+
+let selectedColor = null
+console.log(selectedColor, typeof(selectedColor))
+
+let largeNumber = 546684654848446546484846848468484846549884444446548n;
+console.log(largeNumber, typeof(largeNumber))
+
+
+let person = {name : "John", age : 30, isAdmin : false};
+console.log(person, typeof(person))
+
+let bookTitle = "The Great Boy";
+let numberOfPages = 180;
+let isAvailable = true;
+let publishedYear = undefined;
+let author =null ;
+
+let book = {
+    title : "The Greak Boy",
+    numberOfPages : 180,
+    isAvailable : false,
+    publishedYear : undefined,
+    author : null
+}
