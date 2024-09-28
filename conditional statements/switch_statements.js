9c9da554c8dae54cf8372fcd8e8e2d836ce5bbd3
64-Squares admin <shaikhimran7585@.com>
2024-09-28 21:55:06 +0530

    switch statements in js

---
+++ conditional statements/switch_statements.js
@@ -0,0 +1,34 @@
+
+
+// switch (expression) {
+//     case value1:
+//         //code 
+//         break;
+//     case value2:
+//         // code 
+//         break;
+//     default:
+//         // code
+// }
+
+let day = 10;
+
+switch (day) {
+    case 1:
+        console.log("Its Monday!");
+        break;
+    case 2:
+        console.log("Its Tuesday!");
+        break;
+    case 3:
+        console.log("Its Wednesday!");
+        break;
+    case 4:
+        console.log("Its Thursday!");
+        break;
+    case 5:
+        console.log("Its Friday");
+        break;
+    default:
+        console.log("Its the weekend!")
+}
