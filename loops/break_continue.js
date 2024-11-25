23406151a0fd86e341446f39455a52a6b771ec22
64-Squares admin <shaikhimran7585@.com>
2024-11-25 18:41:52 +0530

    javascript code from mac

---
+++ loops/break_continue.js
@@ -0,0 +1,21 @@
+
+
+// for (let i=1 ; i <= 5; i++){
+//     if (i === 3){
+//         // break; // stop the loop if i is 3
+//         continue; // Skip the rest of the loop is i is 3
+//     }
+//     console.log(i)
+// }
+
+for (let i = 1; i <=10; i++){
+    if ( i===5 ){
+        continue ; // Skip 5
+    }
+
+    if ( i===8 ){
+        break; // Stop at 8
+    }
+
+    console.log(i)
+}
